efa5d1ec2ae99595aaec66a35e7dde5f8ec44a1d
Vitaly <47782238+MyCoRIIIiK@users.noreply.github.com>
2020-07-19 19:43:04 +0300

    Update settings.py

--- TwoHandGame/settings.py
+++ TwoHandGame/settings.py
@@ -161,4 +161,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 import dj_database_url
 db_from_env = dj_database_url.config()
-# DATABASES['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
