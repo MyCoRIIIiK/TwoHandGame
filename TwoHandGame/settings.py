78caa0f6724697a6c9c4cba2cce05baed959f131
Vitaly <47782238+MyCoRIIIiK@users.noreply.github.com>
2020-07-19 18:24:18 +0300

    Update settings.py

--- TwoHandGame/settings.py
+++ TwoHandGame/settings.py
@@ -140,8 +140,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_DIR = (os.path.join(BASE_DIR,'static'))
- STATIC_ROOT = os.path.join(BASE_DIR,'static-files')
- STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR,'static-files')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
